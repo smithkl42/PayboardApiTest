4e2e2c13cbd31f5d856f825179d51fde261580d4
Ken Smith <smithkl42@gmail.com>
2013-09-09 13:35:39 -0700

    Initial commit

---
+++ PayboardApiTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PayboardApiTest.Startup))]
+namespace PayboardApiTest
+{
+    public partial class Startup 
+    {
+        public void Configuration(IAppBuilder app) 
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
